1d370f543039cdf53664dab67f51bc969614c917
Bran van der Meer <bran.van.der.meer@protonmail.com>
2020-03-28 13:34:09 +0100

    feat(setup): Read git user/email from user

--- scripts/setup-dotfiles.bash
+++ scripts/setup-dotfiles.bash
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v git)" ]; then
-    if [ -x "$(command -v apt-get)" ]; then
-        apt-get update
-        apt-get install git -y
-    fi
-    if ! [ -x "$(command -v git)" ]; then
-        printf "\nThis script requires git!\n"
-        exit 1
-    fi
+  if [ -x "$(command -v apt-get)" ]; then
+    apt-get update
+    apt-get install git -y
+  fi
+  if ! [ -x "$(command -v git)" ]; then
+    printf "\nThis script requires git!\n"
+    exit 1
+  fi
 fi
 
 git clone https://github.com/branneman/dotfiles.git ~/dotfiles
 
 symlink() {
-    if [ -e ~/$1 ]; then
-        echo "Found existing file, created backup: ~/${1}.bak"
-        mv ~/$1 ~/$1.bak
-    fi
-    ln -sf ~/dotfiles/$1 ~/$1;
+  if [ -e ~/$1 ]; then
+    echo "Found existing file, created backup: ~/${1}.bak"
+    mv ~/$1 ~/$1.bak
+  fi
+  ln -sf ~/dotfiles/$1 ~/$1;
 }
 
 symlink .profile
@@ -35,5 +35,13 @@ symlink .gitcompletion.bash
 symlink .kubecompletion.bash
 symlink .curlrc
 symlink .hyper.js
+
+echo "Enter Git fullname:"
+read GIT_FULLNAME
+sed -i "s/@@GIT_FULLNAME@@/$GIT_FULLNAME/" .gitconfig
+
+echo "Enter Git email address:"
+read GIT_EMAIL
+sed -i "s/@@GIT_EMAIL@@/$GIT_EMAIL/" .gitconfig
 
 exec $BASH
